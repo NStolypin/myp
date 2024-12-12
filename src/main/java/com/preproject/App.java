0347c8f9dd5cdccf247a2ac8fb36c229a413b989
NStolypin <i@nstolypin.ru>
2024-12-12 22:32:07 +0300

    first

---
+++ src/main/java/com/preproject/App.java
@@ -0,0 +1,13 @@
+package com.preproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
